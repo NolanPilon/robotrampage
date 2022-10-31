74d9fc6f10e685635b30205a0391bf137c5c3fa0
Nolan <npilon2003@gmail.com>
2022-10-31 14:18:41 -0400

    Chapter 10 - Added Robot Animations

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -14,6 +14,7 @@ public class Robot : MonoBehaviour
     private Transform player;
     private float timeLastFired;
     private bool isDead;
+    public Animator robot;
     void Start()
     {
         isDead = false;
@@ -41,6 +42,6 @@ public class Robot : MonoBehaviour
 
     private void fire() 
     {
-        Debug.Log("Fire");
+        robot.Play("Fire");
     }
 }
