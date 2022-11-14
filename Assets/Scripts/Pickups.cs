d170fa43fc18cfe19ebd315a1a63dc31108057d2
Nolan <npilon2003@gmail.com>
2022-11-13 22:48:55 -0500

    Chapter 10 - Added Pickups

---
+++ Assets/Scripts/Pickups.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickups : MonoBehaviour
+{
+    public int type;
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.GetComponent<Player>() != null
+        && collider.gameObject.tag == "Player")
+        {
+            collider.gameObject.GetComponent<Player>().PickUpItem(type);
+            Destroy(gameObject);
+        }
+    }
+}
