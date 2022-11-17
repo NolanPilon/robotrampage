f0585bd8abf3ed6364277bdff09ba65290417a12
Nolan <npilon2003@gmail.com>
2022-11-17 14:46:35 -0500

    Chapter 11 - Added UI Functionality

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     private static Game singleton;
+    public GameUI gameUI;
+    public GameObject player;
+    public int score;
+    public int waveCountdown;
+    public bool isGameOver;
 
     [SerializeField]
     RobotSpawn[] spawns;
@@ -12,6 +17,12 @@ public class Game : MonoBehaviour
     void Start()
     {
         singleton = this;
+        StartCoroutine("increaseScoreEachSecond");
+        isGameOver = false;
+        Time.timeScale = 1;
+        waveCountdown = 30;
+        enemiesLeft = 0;
+        StartCoroutine("updateWaveTimer");
         SpawnRobots();
     }
     private void SpawnRobots()
@@ -20,6 +31,47 @@ public class Game : MonoBehaviour
         {
             spawn.SpawnRobot();
             enemiesLeft++;
+        }
+        gameUI.SetEnemyText(enemiesLeft);
+    }
+    private IEnumerator updateWaveTimer()
+    {
+        while (!isGameOver)
+        {
+            yield return new WaitForSeconds(1f);
+            waveCountdown--;
+            gameUI.SetWaveText(waveCountdown);
+            if (waveCountdown == 0)
+            {
+                SpawnRobots();
+                waveCountdown = 30;
+                gameUI.ShowNewWaveText();
+            }
+        }
+    }
+
+    public static void RemoveEnemy() 
+    {
+        singleton.enemiesLeft--;
+        singleton.gameUI.SetEnemyText(singleton.enemiesLeft);
+        if (singleton.enemiesLeft == 0)
+        {
+            singleton.score += 50;
+            singleton.gameUI.ShowWaveClearBonus();
+        }
+    }
+    public void AddRobotKillToScore()
+    {
+        score += 10;
+        gameUI.SetScoreText(score);
+    }
+    IEnumerator increaseScoreEachSecond()
+    {
+        while (!isGameOver)
+        {
+            yield return new WaitForSeconds(1);
+            score += 1;
+            gameUI.SetScoreText(score);
         }
     }
 }
